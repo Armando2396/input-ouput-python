4308df3b808d4a3af95904e523879105120440d5
Armando2396 <armandomartinezbatista2@gmail.com>
2022-12-28 12:59:00 -0500

    actualizando

--- Ejercicio1-ficheros.py
+++ Ejercicio1-ficheros.py
@@ -2,8 +2,13 @@
 #lo abráis y escribáis dentro del archivo. Para ello, tendréis que acceder dos veces al archivo creado.
 
 f = open("mifichero.txt", 'w')# creamos el fichero
-f.write("hola Mundo")# escribimos en el fichero
+f.write("hola Mundo\n")# escribimos en el fichero
 f.close()# cerramos la consulta al fichero, como buena practica
 
-fichero = open("mifichero.txt")# almacenamos los datos en fichero
-print(fichero.read())# leemos los datos por consola de mifichero.txt
+f = open('mifichero.txt', 'r+')
+f.readline()
+f.write('hola mundo 2\n')
+
+f.seek(0)
+print(f.read())
+f.close()
